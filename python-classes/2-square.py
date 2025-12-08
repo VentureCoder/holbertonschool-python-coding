6685f183794a3887c2e0d969747c1c06033330c0
Orkhan Suleymanov <osul0003@student.monash.edu>
2025-12-08 06:37:13 +0000

    Add 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
+
+class Square:
+    """Represents a square."""
+
     def __init__(self, size=0):
+        """Initialize a new square."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
+        """Return the area of the square."""
         return self.__size * self.__size
